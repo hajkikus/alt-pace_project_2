530fac9568f9c96c7c823d40f908a0777cbe0a0b
Hayk Melkumyan <65418765+hajkikus@users.noreply.github.com>
2023-02-16 01:44:53 +0400

    Add files via upload
    Alt pace challenge project 2

---
+++ project_2.py
@@ -0,0 +1,34 @@
+from random import randrange
+from time import sleep
+
+def rand_dice():
+    m, n = randrange(1, 7), randrange(1, 7)
+    print(f'The sum of dice is {m} + {n} = {m + n}')
+    return m + n
+
+player_wins = [7, 11]
+casino_wins = [2, 3, 12]
+a= rand_dice()
+
+if a in player_wins:
+    print("You won")
+elif a in casino_wins:
+    print("You lose")
+else:
+    print(f'Now your goal number is {a}')
+    player_wins = a
+    casino_wins = 7
+    sleep(1.5)
+    
+    while True:
+        a = rand_dice()
+        if a == player_wins:
+            print("You won")
+            break
+        elif a == casino_wins:
+            print("You lose")
+            break
+        sleep(1.5)
+   
+
+    
